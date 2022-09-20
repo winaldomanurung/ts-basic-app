8c30298a163e7d5ce00188b915253c832768ae74
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-20 08:59:04 +0700

    add popup window to map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -27,12 +27,20 @@ export class CustomMap {
 
   addMarker(mappable: Mappable): void {
     // Marker butuh sesuatu untuk parameternya berupa object dengan interface MarkerOptions
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: "Hi there!",
+      });
+
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
